ca15588d4376bb444d248c837bea07909ed6b4d0
Marcos <wgngui14@gmail.com>
2024-05-09 20:25:17 -0300

    first code

---
+++ src/pages/docs.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Docs() {
+    return (
+        <main>
+            <Link href="/" className="bg-blue-500 text-white p-4">
+                voltar
+            </Link>
+        </main>    
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Index() {
+    return (
+        <main>
+            <Link href="/docs">
+                ir para docs 
+            </Link>
+        </main>    
+    )
+}
